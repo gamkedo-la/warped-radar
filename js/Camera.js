11c3a0143a94d6c9733ea6cc6d4252d3f9c4e198
Herleen Dualan <hd09061998@yahoo.com>
2018-11-15 20:43:47 -0800

    debug mode now controls the camera

--- js/Camera.js
+++ js/Camera.js
@@ -1,4 +1,5 @@
 const CAM_SPEED = 10;
+const EDITOR_SPEED = 5;
 
 var camPanX = 0.0;
 var camPanY = 0.0;
@@ -10,30 +11,7 @@ function instantCamFollow() {
     camPanY = player.y - scaledCanvas.height / 2;
 }
 
-function cameraFollow() {
-    var cameraFocusCenterX = camPanX + scaledCanvas.width / 2;
-    var cameraFocusCenterY = camPanY + scaledCanvas.height / 2;
-
-    var playerDistFromCameraFocusX = Math.abs(player.x - cameraFocusCenterX);
-    var playerDistFromCameraFocusY = Math.abs(player.y - cameraFocusCenterY);
-
-    if (playerDistFromCameraFocusX > PLAYER_DIST_FROM_CENTER_BEFORE_CAMERA_PAN_X) {
-        if (cameraFocusCenterX < player.x) {
-            camPanX += CAM_SPEED;
-        } else {
-            camPanX -= CAM_SPEED;
-        }
-    }
-    if (playerDistFromCameraFocusY > PLAYER_DIST_FROM_CENTER_BEFORE_CAMERA_PAN_Y) {
-        if (cameraFocusCenterY < player.y) {
-            camPanY += CAM_SPEED;
-        } else {
-            camPanY -= CAM_SPEED;
-        }
-    }
-
-     instantCamFollow();
-
+function findEdgesOfScreen() {
     // this next code blocks the game from showing out of bounds
     if (camPanX < 0) {
         camPanX = 0;
@@ -48,6 +26,51 @@ function cameraFollow() {
     }
     if (camPanY > maxPanTop) {
         camPanY = maxPanTop;
+    }
+}
+
+function cameraFollow() {
+    var cameraFocusCenterX = camPanX + scaledCanvas.width / 2;
+    var cameraFocusCenterY = camPanY + scaledCanvas.height / 2;
+    if (debugMode) {
+        var canvasRightBoundary = scaledCanvas.width + camPanX;
+        var canvasBottomBoundary = scaledCanvas.height + camPanY;
+        if ((mouseX / PIXELS_PER_SCALE) > scaledCanvas.width - WORLD_W) {
+            camPanX += EDITOR_SPEED;
+        }
+        if ((mouseX / PIXELS_PER_SCALE) < WORLD_W) {
+            camPanX -= EDITOR_SPEED;
+        }
+        if ((mouseY / PIXELS_PER_SCALE) > scaledCanvas.height - WORLD_H) {
+            camPanY += EDITOR_SPEED;
+        }
+        if ((mouseY / PIXELS_PER_SCALE) < WORLD_H) {
+            camPanY -= EDITOR_SPEED;
+        }
+        findEdgesOfScreen();
+    } else {
+        var cameraFocusCenterX = camPanX + scaledCanvas.width / 2;
+        var cameraFocusCenterY = camPanY + scaledCanvas.height / 2;
+
+        var playerDistFromCameraFocusX = Math.abs(player.x - cameraFocusCenterX);
+        var playerDistFromCameraFocusY = Math.abs(player.y - cameraFocusCenterY);
+
+        if (playerDistFromCameraFocusX > PLAYER_DIST_FROM_CENTER_BEFORE_CAMERA_PAN_X) {
+            if (cameraFocusCenterX < player.x) {
+                camPanX += CAM_SPEED;
+            } else {
+                camPanX -= CAM_SPEED;
+            }
+        }
+        if (playerDistFromCameraFocusY > PLAYER_DIST_FROM_CENTER_BEFORE_CAMERA_PAN_Y) {
+            if (cameraFocusCenterY < player.y) {
+                camPanY += CAM_SPEED;
+            } else {
+                camPanY -= CAM_SPEED;
+            }
+        }
+        instantCamFollow();
+        findEdgesOfScreen();
     }
 }
 
--- js/LevelEditor.js
+++ js/LevelEditor.js
@@ -4,10 +4,26 @@ var levelEditor = new(function () {
     var tileUnderMouse = null;
     var levelCol = 0;
     var levelRow = 0;
+    var roomTileGroup = 0;
+
+    /*var nothingSet = [TILE_TERRAIN, TILE_MOON_TERRAIN, TILE_MOON_TERRAIN_2, TILE_NEXT_LEVEL];
+    var groundSet = arrayWithRange(100, startAt = 100); // 100 to 199
+    var treeSet = arrayWithRange(100, startAt = 200); // 200 to 299
+    var cliffSet = arrayWithRange(100, startAt = 300); // 300 to 399
+    var waterSet = arrayWithRange(100, startAt = 400); // 400 to 499
+    var pathSet = arrayWithRange(100, startAt = 500); // 500 to 599
+    var moonSet = arrayWithRange(100, startAt = 700); // 700 to 799
+    var animalSet = arrayWithRange(100, startAt = 800); // 800 to 899
+*/
+    //var currentlySelectedSet = nothingSet;
+
+    this.editorKeyHandle = function () {
+
+    }
 
     this.roomTileCoordinate = function () {
         if (debugMode) {
-            tileUnderMouse = getTileIndexAtPixelCoord(mouseX/PIXELS_PER_SCALE + camPanX, mouseY/PIXELS_PER_SCALE + camPanY);
+            tileUnderMouse = getTileIndexAtPixelCoord(mouseX / PIXELS_PER_SCALE + camPanX, mouseY / PIXELS_PER_SCALE + camPanY);
 
             var levelCol = arrayIndexToCol(tileUnderMouse);
             var levelRow = arrayIndexToRow(tileUnderMouse);

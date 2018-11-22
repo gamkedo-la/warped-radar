a24739aefb2123e36ec2726ae84c5c1afbc655c0
stebssbets <32585154+stebssbets@users.noreply.github.com>
2018-11-21 21:43:50 -0500

    WIP: will try to add placeholder inventory items to map and then crack at the inventory menu tomorrow 11/21

--- js/ImageLoading.js
+++ js/ImageLoading.js
@@ -3,6 +3,14 @@ let textArrowSheet = document.createElement("img");
 let choiceCursorPic = document.createElement("img");
 
 let inventoryPic = document.createElement("img");
+let brokenSkateboardPic = document.createElement("img");
+let burnerPhonePic = document.createElement("img");
+let crowbarPic = document.createElement("img");
+let hoodiePic = document.createElement("img");
+let medicalNotebookPic = document.createElement("img");
+let sealedTubePic = document.createElement("img");
+let thumbDrivePic = document.createElement("img");
+let trainTicketPic = document.createElement("img");
 
 let gameBorderPic = document.createElement("img");
 
@@ -50,23 +58,25 @@ function loadImages() {
         {varName: choiceCursorPic, theFile: "choicecursor.png"},
         {varName: gameBorderPic, theFile: "gameBorder.png"},
         {varName: inventoryPic, theFile: "inventory.png"},
-        
+
         //PORTRAITS
         {varName: johnHappyPic, theFile: "john.png"},
         {varName: johnMadPic, theFile: "john2.png"},
         {varName: roseHappyPic, theFile: "rose.png"},
         {varName: roseAnnoyedPic, theFile: "rose2.png"},
-        
+
         //PORTRAIT ANIMS
         {varName: johnMouthAnimatedSheet, theFile: "johnmouthmove.png"},
         {varName: roseMouthAnimatedSheet, theFile: "rosemouthmove.png"},
-        
+
         //OVERWORLD CHARACTER ANIMS
         {varName: johnAnimation, theFile: "johnsprite-sheet.png"},
         {varName: johnIdleAnimation, theFile: "johnspriteidle-sheet.png"},
         {varName: johnSideAnimation, theFile: "johnspriteside-sheet.png"},
         {varName: johnSide45Animation, theFile: "johnspriteside45-sheet.png"},
-        
+
+        //INVENTORY ITEMS
+        {worldType: }
         //TILES
         {worldType: TILE_GROUND, theFile: "ground.png"},
         {worldType: TILE_PLAYERS_TILE, theFile: "playerstarttile.png"},
@@ -74,23 +84,25 @@ function loadImages() {
         {worldType: TILE_SIDEWLAK, theFile: "sidewalk.png"},
         {worldType: TILE_SIDEWALK_RIGHTCORNER, theFile: "sidewalk-cornerpiece-right.png"},
         {worldType: TILE_SIDEWALK_VERTICAL, theFile: "sidewalk-vertical.png"},
-        
+
         //BUILDINGS
         {worldType: TILE_TESTBUILDING_LEFT, theFile: "testbuilding-leftcorner.png"},
         {worldType: TILE_TESTBUILDING_RIGHT, theFile: "testbuilding-rightcorner.png"},
         {worldType: TILE_TESTBUILDING_BOTTOMLEFT, theFile: "testbuilding-bottomleftcorner.png"},
         {worldType: TILE_TESTBUILDING_BOTTOMRIGHT, theFile: "testbuilding-bottomrightcorner.png"},
-        
+
         {worldType: TILE_TEST_CONVENIENCE_STORELEFT, theFile: "test-convenience-leftcorner.png"},
         {worldType: TILE_TEST_CONVENIENCE_STOREMIDDLE, theFile: "test-convenience-middle.png"},
         {worldType: TILE_TEST_CONVENIENCE_STORERIGHT, theFile: "test-convenience-rightcorner.png"},
         {worldType: TILE_TEST_CONVENIENCE_STOREBOTTOMLEFT, theFile: "test-convenience-leftbottomcorner.png"},
         {worldType: TILE_TEST_CONVENIENCE_STOREBOTTOMMIDDLE, theFile: "test-convenience-middlebottom.png"},
         {worldType: TILE_TEST_CONVENIENCE_STOREBOTTOMRIGHT, theFile: "test-convenience-rightbottomcorner.png"}
+
+
     ];
-    
+
     picsToLoad = imageList.length;
-    
+
     let loadResult = false;
     for(let i=0;i<imageList.length;i++) {
 		if(imageList[i].varName != undefined) {
@@ -99,7 +111,6 @@ function loadImages() {
 			loadResult = loadImageForWorldCode(imageList[i].worldType, imageList[i].theFile);
 		}
     }
-    
+
     return loadResult;
 }
-

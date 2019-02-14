9e96e52ef7561a8dca0ac6b2f9c1d27a1cd3da18
Chris DeLeon <gamkedo@gmail.com>
2019-02-14 10:30:11 -0800

    Credits (as of today)

--- js/Menu.js
+++ js/Menu.js
@@ -18,6 +18,7 @@ const Menu = new (function() {
     let keyRepeatWait = 0;
 
     let textFontFace = "40px Kelly Slab";
+    let creditsFontFace = "10px Kelly Slab";
     let textColour = "#5a8488" ;
 
     let menuList = ["Story", "Options", "Guide" , "Credits"];
@@ -26,7 +27,46 @@ const Menu = new (function() {
     let optionsList = ["Mute", "Back"];
     let guideList= ["Gameplay","Gamepad", "Back"]; //skip
     let pausedList =['resume', 'mute'];
-    let creditsList =["Kise: Project lead", "BackSpace"];
+    let creditsList =[
+"                                                                                              WARPED RADAR TEAM",
+" ",
+"Kise - Main concept and project co-lead, base functionality for prototype, John character, dialog system code and original format, inventory code, pixel doubling,",
+"  panning camera, room loading, dialog mouth animations, animation code, level editor, store front art, multiple choice dialog feature, cutscene code, John and Rose dialog portraits,",
+"  fade effect, Julie's store song",
+"Ryan Malm - Project co-lead, city tile and building art, decorative detail tiles art, overworld and interiors level design, depth layer layout, doorway arch effect, story revision,",
+"  bloom implementation, assorted bugfixes",
+"H Trayford - Dialog editor (development authoring tool), dialog transition arrows, tile and object culling render optimizations, character-to-character collisions, depth sorting",
+"  implementation, enter/exit room functionality, character integration (Rose, Julie, Uncle Dave, NPC Agent, Cat Man, Cop), event manager, conversation tweaks, family photo code,",
+"  character motion fixes, NPC movement implementation, obtainable items improvements, accusation functionality",
+"Stebs - City background song, inventory items including motherboard and memory chip (early design), web audio implementation, code reorganization, car horns and other",
+"  ambient city noise, volume mixing, song integration, additional bug fixes",
+"Jeff \"Axphin\" Hanlon - Character designer template, John directional animations, Rose directional animations, speed tuning, welcome sign, Dave sprite improvements, cat portrait",
+"  sprite, fusion character portrait",
+"Vaan Hope Khani - Debug mode, pause, vehicle art, menu code, custom font selection, furniture art, mute functionality",
+"Christer \"McFunkypants\" Kaitila - WASD and gamepad input, weather effects, agent portrait, animation bugfix, dust particles, diagonal wall sliding, collision optimizations, car",
+"  movement, intro and endgame text scroller, talking text fade, tile gap fix",
+"Randy Tan Shaoxian - responsive CSS, level editor improvements, input code improvements, additional refactoring and cutscene code organization, cutscene timing tuning",
+"Jaime Rivas - Wall collision code, cat character sprite",
+"Marc Silva - Agent sprite animations, Cop sprite animations, Cop portrait",
+"Michelly Oliveira - Dialog starts when near character, pause when clicked away, in-game labels implementation, help screen",
+"Chris Markle - End Scene/Credits roll song, Main Menu song, Mysterious-sounding song, mechanical grinding sounds",
+"Charlene A. - Julie portrait, Julie sprite animations",
+"Trolzie - Screen transition effects, main menu and game start music integration, cursor fixes",
+"Praneil Kamat - Inventory text wrap, inventory audio fix, browser sound compatibility improvements",
+"Klaim (A. Joël Lamotte) - John's room/intro song, and Dave's House song",
+"Cecilia Borges - Serendipity song",
+"Shaun Lindsley - Synthwave song, busy street sounds",
+"Kirvee - Walking speed adjustments, text editing",
+"Terrence McDonnell - Collision debugging visualization",
+"Jeremy Jackson - Item pickup notification",
+"Brian J. Boucher - Street lamp, fixed dialog/collision related bug",
+"Rémy Lapointe - Additional word wrap fixes",
+"Chris DeLeon - Title and ending voxel renders, compiled credits",
+"  ",
+"                              Game created by worldwide collaborators in Gamkedo Club -- for more info on joining or free games to play visit gamkedo.com",
+"  ",
+"                                                                               CLICK ANYWHERE TO RETURN TO MAIN MENU"
+    ];
     let gameplayList = ["      Move John", "W/Up Arrow: up", "D/Right Arrow: right", "S/Down Arrow: down", "A/Left Arrow: left", "     Game Actions", "P: pause/resume", "Enter/Space: interact", "Z: inventory"];
 
     let menuPageText = [menuList, continueList, optionsList, guideList, creditsList, episodeList, pausedList, gameplayList];
@@ -72,7 +112,7 @@ const Menu = new (function() {
     };
 
     this.checkState = function() {
-        if (currentPage == GAMEPLAY_PAGE) {
+        if (currentPage == GAMEPLAY_PAGE || currentPage == CREDITS_PAGE) {
             currentPage = MENU_PAGE;
             cursor1 =0;
             return;
@@ -179,12 +219,14 @@ const Menu = new (function() {
     }
         if(currentPage == CREDITS_PAGE) {
             colorRect( 0, 0, gameCanvas.width, gameCanvas.height, "black", 0.2);
-            let creditsX = 11;
+            let creditsX = 5;
             let creditsTopY = 25;
-            let creditsLineSkipY = 55;
+            let creditsLineSkipY = 15;
+
             for (let i = 0; i < creditsList.length; i++) {
-                colorText(creditsList[i],creditsX, creditsTopY + creditsLineSkipY * i, textColour, textFontFace, 'left', 'top');
+                colorText(creditsList[i],creditsX, creditsTopY + creditsLineSkipY * i, textColour, creditsFontFace, 'left', 'top');
             }
+            return;
         }
             for (let i=0; i<menuPageText[currentPage].length; i++){
                 colorText(menuPageText[currentPage][i], itemsX - (currentPage == GAMEPLAY_PAGE ? 275 : 0),topItemY + rowHeight * i,textColour, textFontFace, 'left', 'top');
